acc114b7350011b3152420f14193420ce51404e4
Minh <dackhers@gmail.com>
2022-03-17 10:32:25 +0700

    Implement Vigenere

--- vigenere.js
+++ vigenere.js
@@ -1,34 +1,34 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-const vigenere = (plainText, key) => {
+export const vigenere = (plainText, key, isRepeatKey) => {
     let cipherText = ''
-    let p,k,c
+    let plainTextIndex, keyIndex, cipherIndex
+    let finalKey
 
-    let m = key.length
+    isRepeatKey ? finalKey = generateRepeatKey(key, plainText.length, key.length) :  finalKey = generateAutoKey(key, plainText)
 
-    const finalKey = generateKey(key, plainText.length, m)
-
-    for (let i = 0; i<plainText.length; i++) {
-        p = alphabet.indexOf(plainText[i])
-        k = alphabet.indexOf(finalKey[i % m])
-        c = (p + k) % 26
-        cipherText += alphabet[c]
+    for (let i = 0; i < plainText.length; i++) {
+        plainTextIndex = alphabet.indexOf(plainText[i].toLowerCase())
+        keyIndex = alphabet.indexOf(finalKey[i].toLowerCase())
+        cipherIndex = (plainTextIndex + keyIndex) % alphabet.length
+        cipherText += alphabet[cipherIndex].toUpperCase()
     }
-        console.log(cipherText)
+
+
     return cipherText
 }
 
-const generateKey = (key, length, size) => {
+const generateRepeatKey = (key, planTextlength, keyLength) => {
     let finalKey = ''
     let tempKey = key.split('')
     
     
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < planTextlength; i++) {
         finalKey += tempKey[i]
-        tempKey[size + i] = tempKey[i]
+        tempKey[keyLength + i] = tempKey[i]
     }
 
     return finalKey
 }
 
-vigenere('wearediscoverdsaveyourself', 'deceptive')
+const generateAutoKey = (key, plainText) => key + plainText
